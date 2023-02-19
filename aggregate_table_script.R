e0c0b746eec1de0da688041946d8bc74f8757239
Twidelight <1973537598@qq.com>
2023-02-19 15:03:59 -0800

    first draft summary and aggregate

---
+++ aggregate_table_script.R
@@ -0,0 +1,16 @@
+library("dplyr")
+# get the data
+tuesdata <- tidytuesdayR::tt_load('2021-01-26')
+tuesdata <- tidytuesdayR::tt_load(2021, week = 5)
+plastics <- tuesdata$plastics
+
+aggregated <- plastics %>% group_by(country) %>% select(country, empty, hdpe, ldpe, pet, pp, ps, pvc, o, grand_total)
+aggregated <- aggregated %>% rename(empty_count = empty, 
+                                    high_density_polyethylene_count = hdpe, 
+                                    low_density_polyethylene_count = ldpe,
+                                    polyester_plastic_count = pet, 
+                                    polypropylene_count = pp, 
+                                    polystyrene_count = ps,
+                                    PVC_plastic_count = pvc,
+                                    other_count = o,
+                                    total_count = grand_total)
---
+++ summary_information.R
@@ -0,0 +1,17 @@
+library("dplyr")
+# Get the original data from Tidy Tuesday
+install.packages("tidytuesdayR")
+tuesdata <- tidytuesdayR::tt_load('2021-01-26')
+tuesdata <- tidytuesdayR::tt_load(2021, week = 5)
+
+plastics <- tuesdata$plastics
+
+# Summarize information
+summary_info <- list()
+summary_info$num_of_countries <- n_distinct(plastics$country)
+summary_info$max_total_count <- plastics %>% summarize(value = max(grand_total, na.rm = TRUE)) %>% pull(value)
+# high density polyethylene is the material of plastic milk containers, plastic bags, bottle caps, trash cans, oil cans, plastic lumber, toolboxes, supplement containers)
+summary_info$max_high_density_polyethylene_count <- plastics %>% summarize(value = max(hdpe, na.rm = TRUE)) %>% pull(value)
+# polyester plastic is the material of polyester fibers, soft drink bottles, food containers and some plastic bottles. 
+summary_info$max_polyester_plastic_count <- plastics %>% summarize(value = max(pet, na.rm = TRUE)) %>% pull(value)
+summary_info$max_num_volunteer <- plastics %>% summarize(value = max(volunteers, na.rm = TRUE)) %>% pull(value)
